db63ba7b3771e72dff7a0302cb5fd607d4612030
Haley Huang <huangha@us.ibm.com>
2021-07-26 17:03:45 -0400

    additional comments added

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,1 +1,13 @@
 //TODO: Add tipping function and attach to calculate button.
+//Requirements:
+// 1. Create a calculateTip function
+//      calculateTip should consider the Bill Amount and Service Quality when making a calculation
+//      Ensure Bill Amount and Service Quality are not empty before doing any calculation
+//      Round to the nearest 2 decimal places
+// 2. Change the button color to green and hovers blue
+
+
+// Assigns calculateTip function to calculate button
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+};
